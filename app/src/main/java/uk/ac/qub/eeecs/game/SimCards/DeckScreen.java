906a376d31f0e0186d34995b21cf38a2de609e8e
40235126 <jmcdonald28@qub.ac.uk>
2020-02-04 17:58:41 +0000

    Started deck screen

---
+++ app/src/main/java/uk/ac/qub/eeecs/game/SimCards/DeckScreen.java
@@ -0,0 +1,71 @@
+package uk.ac.qub.eeecs.game.SimCards;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+
+import uk.ac.qub.eeecs.gage.Game;
+import uk.ac.qub.eeecs.gage.engine.AssetManager;
+import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
+import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
+import uk.ac.qub.eeecs.gage.world.GameObject;
+import uk.ac.qub.eeecs.gage.world.GameScreen;
+
+public class DeckScreen extends GameScreen {
+
+    private GameObject mBackground;
+
+    public DeckScreen(Game game) {
+        super("DeckScreen", game);
+
+        mDefaultLayerViewport.set(getScreenWidth() / 2, getScreenHeight() / 2, getScreenWidth() / 2, getScreenHeight() / 2);
+        mDefaultScreenViewport.set(0, 0, (int) mDefaultLayerViewport.halfWidth * 2, (int) mDefaultLayerViewport.halfHeight * 2);
+
+        // Load in the bitmaps used on the main menu screen
+        AssetManager assetManager = mGame.getAssetManager();
+        assetManager.loadAndAddBitmap("Background", "img/SimCardsMenuBackground.png");
+
+        //Define Background
+        mBackground = new GameObject(mDefaultLayerViewport.halfWidth,
+                mDefaultLayerViewport.halfHeight, mDefaultLayerViewport.halfWidth * 2, mDefaultLayerViewport.halfHeight * 2, getGame()
+                .getAssetManager().getBitmap("Background"), this);
+    }
+
+    // /////////////////////////////////////////////////////////////////////////
+    // Methods
+    // /////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Update the menu screen
+     *
+     * @param elapsedTime Elapsed time information
+     */
+    @Override
+    public void update(ElapsedTime elapsedTime) {}
+
+
+
+
+    /**
+     * Draw the menu screen
+     *
+     * @param elapsedTime Elapsed time information
+     * @param graphics2D  Graphics instance
+     */
+    @Override
+    public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
+
+        // Clear the screen and draw the buttons
+        graphics2D.clear(Color.BLACK);
+
+        //Draw Background
+        mBackground.draw(elapsedTime, graphics2D, mDefaultLayerViewport, mDefaultScreenViewport);
+    }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels + 128;
+    }
+
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+}
