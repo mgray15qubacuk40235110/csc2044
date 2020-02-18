7f7d7c662c41a285794778988bba24ca65a5c3b0
40235126 <jmcdonald28@qub.ac.uk>
2020-02-18 18:16:05 +0000

    View deck screen now complete

--- app/src/main/java/uk/ac/qub/eeecs/game/SimCards/DeckScreen.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/SimCards/DeckScreen.java
@@ -2,6 +2,7 @@ package uk.ac.qub.eeecs.game.SimCards;
 
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Paint;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,13 +11,15 @@ import uk.ac.qub.eeecs.gage.Game;
 import uk.ac.qub.eeecs.gage.engine.AssetManager;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
+import uk.ac.qub.eeecs.gage.engine.input.TouchEvent;
 import uk.ac.qub.eeecs.gage.ui.PushButton;
 import uk.ac.qub.eeecs.gage.world.GameObject;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
-import uk.ac.qub.eeecs.gage.world.LayerViewport;
+import uk.ac.qub.eeecs.gage.engine.input.Input;
 
 public class DeckScreen extends GameScreen {
 
+    //Defines image for background
     private GameObject mBackground;
 
     //Define the back button to return to the main menu
@@ -25,6 +28,17 @@ public class DeckScreen extends GameScreen {
     //Define deck of cards to be displayed
     private List<Card> mCards;
     private Card[] cards = new Card[20];
+
+    //Measures scroll speed
+    float scroll = 0;
+
+    //Manages touch events
+    private TouchEvent lastTouchEvent;
+    int lastTouchEventType;
+    private boolean mTouchIdExists;
+    private boolean scrolling;
+    private float[] originalTouchLocation = new float[2];
+    private float[] releaseTouchLocation = new float[2];
 
     //Defaults
     private static final int DEFAULT_CARD_HEIGHT = (int) (Resources.getSystem().getDisplayMetrics().heightPixels / 2.5);
@@ -44,7 +58,7 @@ public class DeckScreen extends GameScreen {
 
         //Define Background
         mBackground = new GameObject(mDefaultLayerViewport.halfWidth,
-                mDefaultLayerViewport.halfHeight, mDefaultLayerViewport.halfWidth * 2, mDefaultLayerViewport.halfHeight * 2, getGame()
+                mDefaultLayerViewport.halfHeight, mDefaultLayerViewport.halfWidth * 4.4f, mDefaultLayerViewport.halfHeight * 5.4f, getGame()
                 .getAssetManager().getBitmap("Background"), this);
 
         //Back Button
@@ -60,6 +74,7 @@ public class DeckScreen extends GameScreen {
         int xOffset3 = 120;
         int xOffset4 = 120;
 
+        //Layout cards in rows
         for (int i = 0; i < 20; i++) {
             if (i < 5) {
                 cards[i] = new Card(mDefaultScreenViewport.left + 100 + xOffset,mDefaultScreenViewport.bottom - 270, DEFAULT_CARD_WIDTH, DEFAULT_CARD_HEIGHT, this);
@@ -86,15 +101,6 @@ public class DeckScreen extends GameScreen {
         mDefaultLayerViewport.set(getScreenWidth() / 2, getScreenHeight() / 2, getScreenWidth() / 2, getScreenHeight() / 2);
         mDefaultScreenViewport.set(0, 0, (int) mDefaultLayerViewport.halfWidth * 2, (int) mDefaultLayerViewport.halfHeight * 2);
 
-        // Setup the screen viewport to use the full screen.
-        // mDefaultScreenViewport.set( 0, 0, mGame.getScreenWidth(), mGame.getScreenHeight());
-
-        // Calculate the layer height that will preserved the screen aspect ratio
-        // given an assume 480 layer width.
-        //float layerHeight = mGame.getScreenHeight() * (480.0f / mGame.getScreenWidth());
-
-        //mDefaultLayerViewport.set(240.0f, layerHeight/2.0f, 240.0f, layerHeight/2.0f);
-        //mSpaceLayerViewport = new LayerViewport(240.0f, layerHeight/2.0f, 240.0f, layerHeight/2.0f);
     }
 
     // /////////////////////////////////////////////////////////////////////////
@@ -109,11 +115,18 @@ public class DeckScreen extends GameScreen {
     @Override
     public void update(ElapsedTime elapsedTime) {
 
-        mBackButton.update(elapsedTime);
+        // Process any touch events occurring since the last update
+        Input input = mGame.getInput();
 
+        //Updating back button and checking if tapped
+        mBackButton.update(elapsedTime);
         if (mBackButton.isPushTriggered()) {
             mGame.getScreenManager().removeScreen(this);
         }
+
+        //Updating screen with any scrolling actions
+        checkScroll(input);
+
     }
 
     /**
@@ -140,9 +153,7 @@ public class DeckScreen extends GameScreen {
 
         //Draw Button
         mBackButton.draw(elapsedTime, graphics2D, mDefaultLayerViewport, mDefaultScreenViewport);
-
     }
-
 
     public static int getScreenWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels + 128;
@@ -151,4 +162,72 @@ public class DeckScreen extends GameScreen {
     public static int getScreenHeight() {
         return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
-}
+
+    public void checkScroll(Input input) {
+
+        //Getting touch events since last update
+        List<TouchEvent> touchEvents = input.getTouchEvents();
+        if (touchEvents.size() > 0) {
+            lastTouchEvent = touchEvents.get(touchEvents.size() - 1);
+            lastTouchEventType = lastTouchEvent.type;
+        }
+
+        //Checking if either the user is scrolling or, if they're not, have they released their finger
+        mTouchIdExists = input.existsTouch(0);
+        if (mTouchIdExists && !scrolling) {
+            originalTouchLocation[0] = input.getTouchX(0);
+            originalTouchLocation[1] = (mDefaultLayerViewport.halfHeight * 2.0f) - input.getTouchY(0);
+            if (touchEvents.size() > 0) {
+                if (lastTouchEventType == 2 || lastTouchEventType == 6) {
+                    scroll = 0;
+                    scrolling = true;
+                }
+            }
+        } else {
+            for (TouchEvent indexTouchEvent : touchEvents) {
+                if (indexTouchEvent.type == 1) {
+                    scrolling = false;
+                }
+            }
+        }
+
+        //This runs when the user is scrolling with their finger
+        if(scrolling) {
+            //Get current finger location
+            releaseTouchLocation[1] = (mDefaultLayerViewport.halfHeight * 2.0f) - input.getTouchY(0);
+            //Different circumstances, is user scrolling down or up, once they've stopped scrolling slow down the scroll
+            if (releaseTouchLocation[1] > originalTouchLocation[1]) {
+                scroll -= 3.0f;
+            }
+            else if (releaseTouchLocation[1] < originalTouchLocation[1]) {
+                scroll += 3.0f;
+            }
+            else if (scroll > 0) {
+                scroll = scroll - 1.5f;
+            }
+            else if (scroll < 0) {
+                scroll = scroll + 1.5f;
+            }
+        }
+
+        //Keeps screen in upper and lower bounds
+        if (mDefaultLayerViewport.getBottom() < -830) {
+            mDefaultLayerViewport.y -= (mDefaultLayerViewport.getBottom() + 830);
+            scroll = 0;
+            scrolling = false;
+        }
+        else if (mDefaultLayerViewport.getTop() > 1080) {
+            mDefaultLayerViewport.y -= (mDefaultLayerViewport.getTop() - 1080);
+            scroll = 0;
+            scrolling = false;
+        }
+
+        //Applying the calculated scroll to the screen
+        mDefaultLayerViewport.y += scroll;
+
+        //Keep back button in same position at all times
+        mBackButton.setPosition(mDefaultLayerViewport.getRight() - 100, mDefaultLayerViewport.y - 430);
+
+        }
+
+    }
